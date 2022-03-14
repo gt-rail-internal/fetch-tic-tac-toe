52bcf5b0f5dc3b59dcac17a152650cf5e902e10e
Kartik Ramachandruni <kvr6@gatech.edu>
2022-03-14 17:38:22 -0400

    Make moveit group commander into a global var

--- test_motion.py
+++ test_motion.py
@@ -3,8 +3,6 @@ from math import pi
 from motion import *
 
 def init_arm():
-    initialize_motion()
-
     #joints = [2.9803242683410645, -1.5093436805688476, -0.4041502734541504, -1.129718886820984,-1.4648575595581055, 1.9692782062088012, 2.017528761517334, 0.6389203225610351]
     
     # init forward right near 0,1
