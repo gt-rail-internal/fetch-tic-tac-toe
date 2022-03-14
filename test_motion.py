2367e9b9843e4cc768a9906c40f4e915e1c13787
Kartik Ramachandruni <kvr6@gatech.edu>
2022-03-14 17:22:24 -0400

    Error in importing move folder

--- test_motion.py
+++ test_motion.py
@@ -1,6 +1,6 @@
 from math import pi
 
-from move import motion
+from move.motion import *
 
 def init_arm():
     #joints = [2.9803242683410645, -1.5093436805688476, -0.4041502734541504, -1.129718886820984,-1.4648575595581055, 1.9692782062088012, 2.017528761517334, 0.6389203225610351]
@@ -8,7 +8,7 @@ def init_arm():
     # init forward right near 0,1
     joints = [2.9, -.6, -pi/4, 0, 4*pi/8, 0, pi/4, 0]
     
-    motion.move_arm_joints(joints)
+    move_arm_joints(joints)
     print("Arm location initialized")
 
 def main():
