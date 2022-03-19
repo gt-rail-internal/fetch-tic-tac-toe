e26566f00f79300250b966f2eebb202c568c00ae
Kartik <kartik.rv96@gmail.com>
2022-03-18 22:27:10 -0400

    ros vision code

---
+++ vision/vision_ros.py
@@ -0,0 +1,27 @@
+import rospy
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+from vision.vision import process_image
+
+cvBridge = CvBridge()
+
+def callback(msg):
+
+    # Convert rosmsg to opencv image
+    try:
+        cv_image = cvBridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+
+    # Call process_image function
+    process_image(cv_image)
+
+if __name__ == '__main__':
+    # Initialize ros node
+    rospy.init_node('vision_ros')
+
+    # Image subscriber and spin
+    rospy.Subscriber('/head_camera/rgb/image_raw', Image, callback, queue_size=1)
+    rospy.spin()
